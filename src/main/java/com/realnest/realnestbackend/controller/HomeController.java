8a6208d450a4fe05ee1086fd3dbfb049af10fb71
chouhanneha7150-del <chouhanneha7150@gmail.com>
2026-03-27 22:15:52 +0530

    fixed port + removed local mongobd

--- src/main/java/com/realnest/realnestbackend/controller/HomeController.java
+++ src/main/java/com/realnest/realnestbackend/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.realnest.realnestbackend.controller; 
+package com.realnest.realnestbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
